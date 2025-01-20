d0b70d1ba9cc0a82e8252cd231599467798f2584
AlRizsyad <malrizsyad@gmail.com>
2025-01-20 14:39:26 +0700

    relationship clear

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Pinjam;
+use App\Models\Ulasan;
+use App\Models\KategoriBuku;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Buku extends Model
 {
     use HasFactory;
+
+    public function katgori_buku()
+    {
+        return $this->belongsTo(KategoriBuku::class);
+    }
+
+    public function pinjams()
+    {
+        return $this->hasMany(Pinjam::class);
+    }
+
+    public function ulasans()
+    {
+        return $this->hasMany(Ulasan::class);
+    }
 }
